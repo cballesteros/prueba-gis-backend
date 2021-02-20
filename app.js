a3c347d6094ad01ad2bfbbbf4dc201a2a0815e1c
Cristian Ballesteros <cballest@pros.com>
2021-02-20 14:14:42 -0500

    test deploy cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "[https://enigmatic-oasis-46978.herokuapp.com/]");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
